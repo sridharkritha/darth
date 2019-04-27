02e9ed4f6a573bf59895c56702afbd1484e2dd1b
sridharkritha <sridharkritha@gmail.com>
2019-04-27 16:00:01 +0100

    Bug fixes

--- src/main.js
+++ src/main.js
@@ -46,8 +46,51 @@
 		betMinutesOffset = 300; // place bet: +1 min before the start time, -5 min after the start time		
 		whichDayEvent = 'today'; // 'today'   or    'tomorrow'
 	}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// remote.js
+var express = require('express');
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+remoteHost = function(callback, printCallback) {
+	app.get('/sri', function (req, res) {
+
+		//res.send('<html><body><h1>Hello World</h1></body></html>');
+		//res.send({a: str});
+		publishString = printCallback();
+		res.send(JSON.stringify(publishString));
+	});
+
+	app.get('/', function (req, res) {
+		//res.send('<html><body><h1>Hello World</h1></body></html>');
+		res.sendFile('index.html', {root: __dirname });
+	});
+
+	app.post('/submit-student-data', function (req, res) {
+		//var name = req.body.firstName + ' ' + req.body.lastName;
+
+		var credential = {};
+		credential.username = req.body.firstName;
+		credential.password = req.body.lastName;
+
+		return callback(credential);
+	});
+
+	var port = process.env.PORT || 1229;
+	var server = app.listen(port, function () {
+		console.log('Node server is running..');
+	});
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	getSessionStartTime = function() {
 		return sessionStartTime;
 	}
@@ -701,7 +744,8 @@
 
 	getNewSession = function() {
 		//ACCESS.getAccess('local', loginCallback, null); // login
-		ACCESS.getAccess('remote', loginCallback, printCallback); // login
+		//ACCESS.getAccess('remote', loginCallback, printCallback); // login
+		remoteHost( function(credential) { this.login(null, credential, loginCallback);}, printCallback);
 	};
 
 	// Entry Function
